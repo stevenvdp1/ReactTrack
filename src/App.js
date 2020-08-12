154830d2d7add9c8695217796d0a1925ee478934
Steven Van de Poel <stevenvandepoel@gmail.com>
2020-08-12 10:36:04 +0200

    helloworld

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import HelloWorld from './components/HelloWorld.functional';
+// import HelloWorld from './components/HelloWorld.class';
 
 function App() {
   return (
     <div className="App">
-
+      <HelloWorld name={"Steven"}/>
     </div>
   );
 }
