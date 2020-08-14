697eb47a10c8a34968a5e0edfe6254cbaa01e05e
Steven Van de Poel <stevenvandepoel@gmail.com>
2020-08-14 19:02:06 +0200

    namechange

---
+++ src/Components/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Body(){
+    return(
+        <div>
+            content
+        </div>
+    )
+}
+
+export default Body
---
+++ src/Components/SideMenu.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const styles = {
+    menu: {
+        position: 'fixed',
+        backgroundColor: 'white',
+        top: 0,
+        left: 0,
+        width: '75vw',
+        height: '100vh',
+        borderRight:'rgba(1,1,1,0.3) solid 2px',
+    },
+    mask: {
+        position: 'fixed',
+        backgroundColor: 'grey',
+        opacity:'0.6',
+        top: 0,
+        left: '75vw',
+        width: '25vw',
+        height: '100vh'
+    }
+}
+
+
+function SideMenu(props) {
+    return (
+        <React.Fragment>
+            <div style={styles.menu}>menu</div>
+            <div style={styles.mask} onClick={props.setShowMenu}/>
+        </React.Fragment>
+    )
+}
+
+export default SideMenu
