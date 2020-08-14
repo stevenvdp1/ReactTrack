cf508dc663fd0625cbe41f860215120731d3b99c
Steven Van de Poel <stevenvandepoel@gmail.com>
2020-08-14 17:22:17 +0200

    header footer boyd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import './App.css';
-// import Counter from './components/Counter.functional';
-import Counter from './components/Counter.class';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Body from './components/Body';
 
+const styles={
+  container:{
+    padding:0,
+    margin:0,
+    height:'100vh',
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'space-between'
+  }
+}
 function App() {
   return (
-    <div className="App">
-        <Counter/>
+    <div style={styles.container}>
+      <Header/>
+      <Body/>
+      <Footer/>
     </div>
   );
 }
